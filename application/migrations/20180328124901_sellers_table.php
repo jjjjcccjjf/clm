f752196bdbc9eec37cd3a8c4410d01a4080149bc
Lorenzo Salamante <lsalamante@myoptimind.com>
2018-04-13 10:27:19 +0800

    Update initial data

--- application/migrations/20180328124901_sellers_table.php
+++ application/migrations/20180328124901_sellers_table.php
@@ -1,4 +1,4 @@
-  <?php
+<?php
 
 defined('BASEPATH') OR exit('No direct script access allowed');
 
@@ -62,88 +62,196 @@ class Migration_sellers_table extends CI_Migration {
         'type' => 'VARCHAR',
         'constraint' => '10000',
         'default' => '{}'
-      ),
-      'forgot_token' => array(
-        'type' => 'TEXT',
-        'null' => true,
-      ),
-      'position_id' => array(
-        'type' => 'INT',
-        'null' => true,
-      ),
-      'division_id' => array(
-        'type' => 'INT',
-        'null' => true,
-      ),
-      'group_id' => array(
-        'type' => 'INT',
-        'null' => true,
-      ),
-      'image_url' => array(
-        'type' => 'TEXT',
-      ),
-      # Pending edits are saved here
-      'pending_payload' => array(
-        'type' => 'VARCHAR',
-        'constraint' => '10000',
-        'default' => '[{},{}]'
-      ),
-      'imported_csv' => array(
-        'type' => 'TEXT',
-        'null' => true
-      ),
-    ));
+        ),
+        'forgot_token' => array(
+          'type' => 'TEXT',
+          'null' => true,
+        ),
+        'position_id' => array(
+          'type' => 'INT',
+          'null' => true,
+        ),
+        'division_id' => array(
+          'type' => 'INT',
+          'null' => true,
+        ),
+        'group_id' => array(
+          'type' => 'INT',
+          'null' => true,
+        ),
+        'image_url' => array(
+          'type' => 'TEXT',
+        ),
+        # Pending edits are saved here
+        'pending_payload' => array(
+          'type' => 'VARCHAR',
+          'constraint' => '10000',
+          'default' => '[{},{}]'
+          ),
+          'imported_csv' => array(
+            'type' => 'TEXT',
+            'null' => true
+          ),
+        ));
 
-    # Table date defaults
-    $this->dbforge->add_field("`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
-    $this->dbforge->add_field("`updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP");
+        # Table date defaults
+        $this->dbforge->add_field("`created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP");
+        $this->dbforge->add_field("`updated_at` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP");
 
 
-    if($this->dbforge->create_table('sellers'))
-    {
-      $table = 'sellers';
+        if($this->dbforge->create_table('sellers'))
+        {
+          $table = 'sellers';
 
-      $data = array(
-        'full_name' => 'Magen Attraglaitz',
-        'birth_date' => '2018-01-01',
-        'password' => password_hash('password', PASSWORD_DEFAULT),
-        'gender' => 'Male',
-        'civil_status' => 'Single',
-        'home_address' => 'Royal Capital, Luspierheil, Attraglaitz Royal Castle',
-        'office_address' => 'Strangaz',
-        'mobile_num' => '09451494315',
-        'office_fax' => 'Hello',
-        'home_num' => '22299222',
-        'email' => 'lsalamante@myoptimind.com',
-        'real_estate_record_type' => 'Broker',
-        'real_estate_record_payload' => '{}',
-        'image_url' => 'https://robohash.org/Magen Attraglaitz?set=set4' ,
-        );
-        $this->db->insert($table, $data);
+          $data = array(
+            'full_name' => 'Magen Attraglaitz',
+            'birth_date' => '2018-01-01',
+            'password' => password_hash('password', PASSWORD_DEFAULT),
+            'gender' => 'Male',
+            'civil_status' => 'Single',
+            'home_address' => 'Royal Capital, Luspierheil, Attraglaitz Royal Castle',
+            'office_address' => 'Strangaz',
+            'mobile_num' => '09451494315',
+            'office_fax' => 'Hello',
+            'home_num' => '22299222',
+            'email' => 'lsalamante@myoptimind.com',
+            'real_estate_record_type' => 'Broker',
+            'real_estate_record_payload' => '{}',
+              'image_url' => 'https://robohash.org/Magen Attraglaitz?set=set4' ,
+            );
+            $this->db->insert($table, $data);
 
-      $data = array(
-        'full_name' => 'Vane',
-        'birth_date' => '2018-01-01',
-        'password' => password_hash('password', PASSWORD_DEFAULT),
-        'gender' => 'Male',
-        'civil_status' => 'Single',
-        'home_address' => 'Unknown',
-        'office_address' => 'Strangaz',
-        'mobile_num' => '09451494311',
-        'office_fax' => 'Hello',
-        'home_num' => '22299222',
-        'email' => 'est@esty.com',
-        'real_estate_record_type' => 'Agent',
-        'real_estate_record_payload' => '{}',
-        'image_url' => 'https://robohash.org/Vane?set=set4' ,
-        );
-        $this->db->insert($table, $data);
+            $data = array(
+              'full_name' => 'Vane',
+              'birth_date' => '2018-01-01',
+              'password' => password_hash('password', PASSWORD_DEFAULT),
+              'gender' => 'Male',
+              'civil_status' => 'Single',
+              'home_address' => 'Unknown',
+              'office_address' => 'Strangaz',
+              'mobile_num' => '09451494311',
+              'office_fax' => 'Hello',
+              'home_num' => '22299222',
+              'email' => 'est@esty.com',
+              'real_estate_record_type' => 'Agent',
+              'real_estate_record_payload' => '{}',
+                'image_url' => 'https://robohash.org/Vane?set=set4' ,
+              );
+              $this->db->insert($table, $data);
 
-      }
-    }
+              $data = array(
+                'full_name' => 'Godfrey',
+                'birth_date' => '1994-01-01',
+                'password' => password_hash('password', PASSWORD_DEFAULT),
+                'gender' => 'Male',
+                'civil_status' => 'Single',
+                'home_address' => 'Unknown',
+                'office_address' => 'Strangaz',
+                'mobile_num' => '09451494371',
+                'office_fax' => 'Hello',
+                'home_num' => '22299222',
+                'email' => 'g@strangaz.com',
+                'real_estate_record_type' => 'Agent',
+                'real_estate_record_payload' => '{}',
+                  'image_url' => 'https://robohash.org/Godfrey?set=set4' ,
+                );
+                $this->db->insert($table, $data);
 
-    public function down()
-    {
-      $this->dbforge->drop_table('sellers');
-    }
-  }
+                $data = array(
+                  'full_name' => 'Sin',
+                  'birth_date' => '1994-01-01',
+                  'password' => password_hash('password', PASSWORD_DEFAULT),
+                  'gender' => 'Male',
+                  'civil_status' => 'Single',
+                  'home_address' => 'Unknown',
+                  'office_address' => 'Strangaz',
+                  'mobile_num' => '09451494371',
+                  'office_fax' => 'Hello',
+                  'home_num' => '22299222',
+                  'email' => 's@strangaz.com',
+                  'real_estate_record_type' => 'Agent',
+                  'real_estate_record_payload' => '{}',
+                    'image_url' => 'https://robohash.org/Sin?set=set4' ,
+                  );
+                  $this->db->insert($table, $data);
+
+                  $data = array(
+                    'full_name' => 'Sid Attraglaitz',
+                    'birth_date' => '1994-01-01',
+                    'password' => password_hash('password', PASSWORD_DEFAULT),
+                    'gender' => 'Male',
+                    'civil_status' => 'Single',
+                    'home_address' => 'Unknown',
+                    'office_address' => 'Strangaz',
+                    'mobile_num' => '09451494371',
+                    'office_fax' => 'Hello',
+                    'home_num' => '22299222',
+                    'email' => 'sid@strangaz.com',
+                    'real_estate_record_type' => 'Agent',
+                    'real_estate_record_payload' => '{}',
+                      'image_url' => 'https://robohash.org/Sid?set=set4' ,
+                    );
+                    $this->db->insert($table, $data);
+
+                    $data = array(
+                      'full_name' => 'Alec D\'Angelo',
+                      'birth_date' => '1994-01-01',
+                      'password' => password_hash('password', PASSWORD_DEFAULT),
+                      'gender' => 'Male',
+                      'civil_status' => 'Single',
+                      'home_address' => 'Unknown',
+                      'office_address' => 'Strangaz',
+                      'mobile_num' => '09451494371',
+                      'office_fax' => 'Hello',
+                      'home_num' => '22299222',
+                      'email' => 'alec@strangaz.com',
+                      'real_estate_record_type' => 'Agent',
+                      'real_estate_record_payload' => '{}',
+                        'image_url' => 'https://robohash.org/Alec-d-angelo?set=set4' ,
+                      );
+                      $this->db->insert($table, $data);
+
+                      $data = array(
+                        'full_name' => 'Rigel Attraglaitz',
+                        'birth_date' => '1994-01-01',
+                        'password' => password_hash('password', PASSWORD_DEFAULT),
+                        'gender' => 'Male',
+                        'civil_status' => 'Single',
+                        'home_address' => 'Unknown',
+                        'office_address' => 'Strangaz',
+                        'mobile_num' => '09451494371',
+                        'office_fax' => 'Hello',
+                        'home_num' => '22299222',
+                        'email' => 'rigel@strangaz.com',
+                        'real_estate_record_type' => 'Agent',
+                        'real_estate_record_payload' => '{}',
+                          'image_url' => 'https://robohash.org/Rigel Attraglaitz?set=set4' ,
+                        );
+                        $this->db->insert($table, $data);
+
+                        $data = array(
+                          'full_name' => 'Book Attraglaitz',
+                          'birth_date' => '1994-01-01',
+                          'password' => password_hash('password', PASSWORD_DEFAULT),
+                          'gender' => 'Male',
+                          'civil_status' => 'Single',
+                          'home_address' => 'Unknown',
+                          'office_address' => 'Strangaz',
+                          'mobile_num' => '09451494371',
+                          'office_fax' => 'Hello',
+                          'home_num' => '22299222',
+                          'email' => 'book@strangaz.com',
+                          'real_estate_record_type' => 'Agent',
+                          'real_estate_record_payload' => '{}',
+                            'image_url' => 'https://robohash.org/Book Attraglaitz?set=set4' ,
+                          );
+                          $this->db->insert($table, $data);
+
+                        }
+                      }
+
+                      public function down()
+                      {
+                        $this->dbforge->drop_table('sellers');
+                      }
+                    }
--- application/migrations/20180410124901_sales_table.php
+++ application/migrations/20180410124901_sales_table.php
@@ -36,35 +36,187 @@ class Migration_sales_table extends CI_Migration {
     {
       $table = 'sales';
 
-      $data = array(
-        'project_name' => 'Test project 1',
-        'date' => '2018-01-01',
-        'seller_id' => 1,
-        'sales_amount' => 3000000,
-      );
-      $this->db->insert($table, $data);
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 1,
+          'sales_amount' => 3000000,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 1,
+          'sales_amount' => 1800000,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 1,
+          'sales_amount' => 200000,
+        )
+      ]
+    );
 
-      $data = array(
-        'project_name' => 'Test project 2',
-        'date' => '2018-01-01',
-        'seller_id' => 1,
-        'sales_amount' => 1800000,
-      );
-      $this->db->insert($table, $data);
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 2,
+          'sales_amount' => 361200,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 2,
+          'sales_amount' => 782000,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 2,
+          'sales_amount' => 78112.90,
+        )
+      ]
+    );
 
-      $data = array(
-        'project_name' => 'Test project 3',
-        'date' => '2018-01-01',
-        'seller_id' => 1,
-        'sales_amount' => 200000,
-      );
-      $this->db->insert($table, $data);
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 3,
+          'sales_amount' => 3821200,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 3,
+          'sales_amount' => 1182000,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 3,
+          'sales_amount' => 2424111,
+        )
+      ]
+    );
 
-    }
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 4,
+          'sales_amount' => 722726,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 4,
+          'sales_amount' => 212329,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 4,
+          'sales_amount' => 824900,
+        )
+      ]
+    );
+
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 5,
+          'sales_amount' => 7249333,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 5,
+          'sales_amount' => 1525126,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 5,
+          'sales_amount' => 1262238,
+        )
+      ]
+    );
+
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 6,
+          'sales_amount' => 811927,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 6,
+          'sales_amount' => 2109280,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 6,
+          'sales_amount' => 9010828,
+        )
+      ]
+    );
+
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 7,
+          'sales_amount' => 1521778,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 7,
+          'sales_amount' => 9503456,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 7,
+          'sales_amount' => 4733930,
+        )
+      ]
+    );
+
+      $this->db->insert_batch($table, [
+        array(
+          'project_name' => 'Test project 1',
+          'date' => '2018-04-13',
+          'seller_id' => 8,
+          'sales_amount' => 1631627,
+        ),
+        array(
+          'project_name' => 'Test project 2',
+          'date' => '2018-04-13',
+          'seller_id' => 8,
+          'sales_amount' => 4680000,
+        ),
+        array(
+          'project_name' => 'Test project 3',
+          'date' => '2018-04-13',
+          'seller_id' => 8,
+          'sales_amount' => 56686900,
+        )
+      ]
+    );
+
   }
+}
 
-  public function down()
-  {
-    $this->dbforge->drop_table('sales');
-  }
+public function down()
+{
+  $this->dbforge->drop_table('sales');
+}
 }
