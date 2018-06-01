ee63f40231010ce6405de24b0a11be8ce90fddde
Lorenzo Salamante <lsalamante@myoptimind.com>
2018-06-01 14:08:45 +0800

    Modify projects template

--- application/views/front/projects.php
+++ application/views/front/projects.php
@@ -1,24 +1,21 @@
+
+
 <!-- Main Dashboard -->
 <article class="maincontent" id="page-content-wrapper">
-  <div class="pagewrapper">
-    <h1>Projects
-    </h1>
-    <ul class="events">
+  <div class="projects">
+    <h1>Projects</h1>
+
+    <ul class="project-list">
       <?php if ($projects): ?>
-        <!-- if there are events -->
         <?php foreach ($projects as $key => $value): ?>
           <li>
-            <article class="reward">
-              <!-- <div>
-                <h3><?php echo $value->title ?></h3>
-              </div> -->
+            <figure>
               <a href="<?php echo base_url('dashboard/project-details/') . $value->id ?>">
                 <img src="<?php echo $value->image_url ?>">
               </a>
-            </article>
+            </figure>
           </li>
         <?php endforeach; ?>
-        <!-- /if there are events -->
       <?php else: ?>
         <li style="width:100%">
           <h3>No projects available</h3>
@@ -28,9 +25,3 @@
   </div>
 </article>
 <!-- End of Main Dashboard -->
-
-
-<script type="text/javascript">
-$(document).ready(function() {
-});
-</script>
